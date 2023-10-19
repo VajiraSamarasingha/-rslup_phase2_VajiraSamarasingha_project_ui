a2e5e2eb9a66e3ab446fe2e65cdbe17012bcb332
G.M.Vajira Samarasingha <85574738+VajiraSamarasingha@users.noreply.github.com>
2023-10-19 16:47:56 +0530

    Add login Authentication
    This is try to add login authentication. when click login button . that username password are correct show allert user loging success and when username password are incorrect that show login failed

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  login(){
+    const username = "Vajira";
+    const password = "Vajira123"
 
+    if (this.username === username && this.password === password){
+      alert("Login Successful");
+    }
+    else{
+      alert("Login Failed");
+    }
+  }
 }
